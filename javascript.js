12eccf661b941102eb4a3af53ec5ecee92e68d26
kaito24mako <kaitowatanabemcc@gmail.com>
2025-09-08 15:15:38 +1000

    Generate number of either 1, 2, or 3
    Changed from 0-1 to 1-3 due to inequalities in probability

--- javascript.js
+++ javascript.js
@@ -1,7 +1,16 @@
+// Computer generates random number and returns string choice
 function getComputerChoice() {
-    const randomNumber = Math.random();
-        if (randomNumber <= 0.33) {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNumber);
+        if (randomNumber === 1) {
             return "Rock";
         }
+        else if (randomNumber === 2) {
+            return "Paper";
+        }
+        else {
+            return "Scissors";
+        }
     }
+getComputerChoice();
 
