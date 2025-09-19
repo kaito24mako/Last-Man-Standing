5eb620c158a15d6726367030be09bc936b0bc94d
kaito24mako <kaitowatanabemcc@gmail.com>
2025-09-19 15:31:26 +1000

    create event listener which outputs correct user input, but is not using the function

--- javascript.js
+++ javascript.js
@@ -52,3 +52,66 @@ function playRound(humanSelection, computerSelection) {
 }
 
 
+// buttons for humanSelection 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const buttons = document.querySelector(".buttons");
+
+let myFunction = playRound; 
+
+buttons.addEventListener("click", (event) => {
+    switch(buttons.id) {
+        case "rock":
+            humanSelection="rock";
+            break;
+        case "paper":
+            humanSelection="paper";
+            break;
+        case "scissors":
+            humanSelection="scissors";
+            break;
+    };
+    console.log(event.target.id);
+});
+
+
+
+
+
+
+
+
+
+// logic for five rounds
+/*
+function playGame() {
+    for(let round = 1; round <= 5; round++) {      // loop calls playRound() 5 times
+           
+        const humanSelection = getHumanChoice();   // functions become variables for playRound() to use as arguments
+        const computerSelection = getComputerChoice();
+
+        console.log(`*********** Round ${round} : FIGHT ***********`);   
+        console.log(`You used *${humanSelection}* `);   
+        console.log(`Your opponent used *${computerSelection}* `);
+
+        playRound(humanSelection, computerSelection);  // arguments allow to compare and determine winner
+       
+        console.log(`Your score: ${humanScore} | Opponent's score: ${computerScore} | Draw score: ${drawScore}`);  
+    } 
+    if (humanScore > computerScore) {
+        console.log("Congratulations!\nYou emerge victorious!!");
+    }
+    else if (computerScore > humanScore) {
+        console.log("Oh no...You have been defeated.\nWill you enter The Arena once more?");
+    }
+    else {
+        console.log("You are both an equal match.\nWill you enter The Arena once more?");
+    }
+}
+
+playGame();
+    */
+
+
+
