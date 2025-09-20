fa05a8ed82182dfa76fa51fe0541ff6850268da2
kaito24mako <kaitowatanabemcc@gmail.com>
2025-09-20 19:32:34 +1000

    put UI based codes into one function for readability

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 const buttons = document.querySelector(".buttons");
+const ui = setupUI();
 
 // Computer generates random number 
 function getComputerChoice() {
@@ -19,49 +20,49 @@ function getComputerChoice() {
         }
     }
 
-// new DOM elements for logic of one round
-let container = document.querySelector(".result");
-let roundText = document.createElement("p");
-let playerText = document.createElement("p");
-let computerText = document.createElement("p");
-let resultText = document.createElement("p");
-let scoreText = document.createElement("p");
-container.append(roundText, playerText, computerText, resultText, scoreText);
+// UI setup
+function setupUI () {
+    let container = document.querySelector(".result");
 
+    let roundText = document.createElement("p");
+    let playerText = document.createElement("p");
+    let computerText = document.createElement("p");
+    let resultText = document.createElement("p");
+    let scoreText = document.createElement("p");
+
+    container.append(roundText, playerText, computerText, resultText, scoreText);
+
+    return {roundText, playerText, computerText, resultText, scoreText};
+};
 
 // Logic of one round
 function playRound(playerSelection, computerSelection) {  
-
-    // round counter
     ++round;
-    roundText.textContent = `****** Round ${round} FIGHT ******`;
+    ui.roundText.textContent = `****** Round ${round} FIGHT ******`;
+    ui.playerText.textContent = `You used *${playerSelection}* `;
+    ui.computerText.textContent = `Your opponent used *${computerSelection}* `;
 
-    // player and computer selections
-    playerText.textContent = `You used *${playerSelection}* `;
-    computerText.textContent = `Your opponent used *${computerSelection}* `;
-
-    // win / lose / draw conditions
     if (playerSelection == "rock" && computerSelection == "scissors" ||
         playerSelection == "paper" && computerSelection == "rock" ||
         playerSelection == "scissors" && computerSelection == "paper") {
-        resultText.textContent = "You won the round!!";
+        ui.resultText.textContent = "You won the round!!";
         ++humanScore;  
     }
     else if (playerSelection == computerSelection) {
-        resultText.textContent = "It is a draw~";
+        ui.resultText.textContent = "It is a draw~";
         ++drawScore;
     }
     else {
-        resultText.textContent = "You lost the round...";
+        ui.resultText.textContent = "You lost the round...";
         ++computerScore;
     }
 
-    // scores
-    scoreText.textContent = `Your score: ${humanScore} | Opponent's score: ${computerScore} | Draw score: ${drawScore}`;
+    ui.scoreText.textContent = `Your score: ${humanScore} | Opponent's score: ${computerScore} | Draw score: ${drawScore}`;
 };
 
-
+// Named function for use with Event Listener
 function handleClick(event) {
+
     // calls one round 
     let playerSelection = event.target.id;
     let computerSelection = getComputerChoice();
@@ -70,45 +71,9 @@ function handleClick(event) {
     // stops event 
     if (humanScore === 5 || computerScore === 5) {
         buttons.removeEventListener("click", handleClick);
-        humanScore > computerScore ? alert("Congratulations! You emerge victorious!!") : alert("Oh no...You have been defeated. Will you enter The Arena once more?");
+        humanScore > computerScore ? alert("Congratulations!\nYou emerge victorious!!") : alert("Oh no...You have been defeated.\nWill you enter The Arena once more?");
     }
 };
 
-// calls one round per button click
+// Calls one round per button click
 buttons.addEventListener("click", handleClick);
-    
-
-// logic for five rounds
-/*
-function playGame() {
-    for(let round = 1; round <= 5; round++) {      // loop calls playRound() 5 times
-           
-        const humanSelection = getHumanChoice();   // functions become variables for playRound() to use as arguments
-        const computerSelection = getComputerChoice();
-
-        console.log(`*********** Round ${round} : FIGHT ***********`);   
-        console.log(`You used *${humanSelection}* `);   
-        console.log(`Your opponent used *${computerSelection}* `);
-
-        playRound(humanSelection, computerSelection);  // arguments allow to compare and determine winner
-       
-        console.log(`Your score: ${humanScore} | Opponent's score: ${computerScore} | Draw score: ${drawScore}`);  
-    } 
-    if (humanScore > computerScore) {
-        console.log("Congratulations!\nYou emerge victorious!!");
-    }
-    else if (computerScore > humanScore) {
-        console.log("Oh no...You have been defeated.\nWill you enter The Arena once more?");
-    }
-    else {
-        console.log("You are both an equal match.\nWill you enter The Arena once more?");
-    }
-}  */
-
-
-    // text for user vs opponent inputs - add later!
-    /*
-        console.log(`*********** Round ${round} : FIGHT ***********`);   
-        console.log(`You used *${humanSelection}* `);   
-        console.log(`Your opponent used *${computerSelection}* `);
-    */
